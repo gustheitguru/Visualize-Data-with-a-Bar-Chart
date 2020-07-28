14f65d36032870a8340e43ecc3cb7fcb81133c90
Gus <gusr@logicliving.com>
2020-07-28 14:03:44 -0500

    adding X and Y Axis with data points

---
+++ script.js
@@ -0,0 +1,94 @@
+// console.log(d3)
+let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+
+let req = new XMLHttpRequest()
+
+let data
+let values = [];
+let heightScale 
+let xScale
+let xAxisScale
+let yAxisScale
+let width = 800;
+let height = 600;
+let padding = 40;
+
+//will allow you to bring in all data into your SVG tag on index.html
+let svg = d3.select('svg');
+
+let drawCanvas = () => {
+	svg.attr('width', width)
+	svg.attr('height', height)
+};
+
+let generateScales = () => {
+
+		heightScale = d3.scaleLinear()
+						.domain([0, d3.max(values, (item) => {
+							return item[1]
+						})])
+						.range([0, height - (2*padding)])
+
+		xScale = d3.scaleLinear()
+				   .domain([0, values.length - 1])
+				   .range([padding, width - padding])
+
+		let datesArray = values.map((item) => {
+			return new Date (item[0])
+		})
+
+		// console.log(datesArray)
+
+
+		xAxisScale = d3.scaleTime()
+                    .domain([d3.min(datesArray), d3.max(datesArray)])
+                    .range([padding, width-padding])			   
+
+		// console.log(xAxisScale)
+
+		yAxisScale = d3.scaleLinear()
+						.domain([0, d3.max(values, (item) => {
+								return item[1]
+								})])
+						.range([height - padding, padding])
+
+
+};
+
+let drawBars = () => {
+
+}
+
+let generateAxes = () => {
+
+	let xAxis = d3.axisBottom(xAxisScale)
+
+	svg.append('g')
+	   .call(xAxis)
+	   .attr('id', 'x-axis')
+	   .attr('transform', 'translate(0,' + (height - padding) + ') ')
+
+	let yAxis = d3.axisLeft(yAxisScale)
+	
+	svg.append('g')
+	   .call(yAxis)
+	   .attr('id', 'y-axis')
+	   .attr('transform', 'translate('+ padding +', 0)')   
+
+}
+
+req.open('GET', url, true)
+req.onload = () => {
+	// console.log(req.responseText)
+	data = JSON.parse(req.responseText);
+	values = data.data;
+	// console.log(values)
+	generateScales()
+	drawCanvas()
+	generateAxes()
+	drawBars()
+}
+
+req.send()
+
+
